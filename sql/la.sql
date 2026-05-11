44dcd8723a3e5bd2840dd5443e1e05b96477f4be
Richard Croker <ra.croker@gmail.com>
2026-05-11 16:32:25 +0100

    amend to include case statement

--- sql/la.sql
+++ sql/la.sql
@@ -1,10 +1,61 @@
 WITH
   vmp_amp AS (
-    SELECT DISTINCT id, nm, id AS vmp FROM dmd.vmp
+    SELECT DISTINCT id, nm, id AS vmp, bnf_code FROM dmd.vmp
     UNION DISTINCT
-    SELECT DISTINCT id, nm, vmp FROM dmd.amp
+    SELECT DISTINCT id, nm, vmp, bnf_code FROM dmd.amp
   )
-SELECT vmp_amp.id, vmp_amp.nm
+SELECT
+  vmp_amp.id,
+  vmp_amp.nm,
+  vmp_amp.vmp,
+  vmp_amp.bnf_code,
+  CASE
+    WHEN
+      COALESCE(vpi.bs_subid, vpi.ing) = 373492002
+      AND route.descr = 'patch.transdermal'
+      AND strnt_nmrtr_val / (COALESCE(strnt_dnmtr_val, 1)) >= 37.5
+      THEN
+        TRUE  -- Fentanyl patches (strengths equal to or higher than 37.5mcg/hour) (ing code)
+    WHEN
+      COALESCE(vpi.bs_subid, vpi.ing) = 387024006
+      AND route.descr LIKE '%modified-release.oral'
+      AND vmp_amp.nm NOT LIKE '%Onexila%40mg%'
+      AND strnt_nmrtr_val / (COALESCE(strnt_dnmtr_val, 1)) >= 30
+      THEN
+        TRUE  -- Oxycodone MR oral preps [excluding Onexila 40mg as 24hr] (strengths equal to or higher than 30mg) (ing code)
+    WHEN
+      COALESCE(vpi.bs_subid, vpi.ing) = 441757005
+      AND route.descr LIKE '%modified-release.oral'
+      AND strnt_nmrtr_val / (COALESCE(strnt_dnmtr_val, 1)) >= 112.5
+      THEN
+        TRUE  -- Tapentadol base substance (strengths equal to or higher than 112.5mg) (ing code)
+    WHEN
+      COALESCE(vpi.bs_subid, vpi.ing) = 387173000
+      AND route.descr LIKE 'patch.transdermal'
+      AND strnt_nmrtr_val / (COALESCE(strnt_dnmtr_val, 1)) >= 39.375
+      THEN
+        TRUE  -- Buprenorphine patches (strengths equal to or higher than 39.375mg/hour) (ing code)
+    WHEN
+      COALESCE(vpi.bs_subid, vpi.ing) = 60886004
+      AND route.descr LIKE '%modified-release.oral'
+      AND vmp_amp.nm NOT LIKE '%MXL%'
+      AND strnt_nmrtr_val / (COALESCE(strnt_dnmtr_val, 1)) >= 45
+      THEN
+        TRUE  -- Morphine Sulfate MR oral preps [excluding MXL as 24hr] (strengths equal to or higher than 45mg) (ing code)
+    WHEN
+      COALESCE(vpi.bs_subid, vpi.ing) = 60886004
+      AND route.descr LIKE '%modified-release.oral'
+      AND vmp_amp.nm LIKE '%MXL%'
+      AND strnt_nmrtr_val / (COALESCE(strnt_dnmtr_val, 1)) >= 90
+      THEN TRUE  -- MXL (strengths equal to or higher than 90mg) (ing code)
+    WHEN
+      COALESCE(vpi.bs_subid, vpi.ing) = 44508008
+      AND route.descr LIKE '%modified-release.oral'
+      AND strnt_nmrtr_val / (COALESCE(strnt_dnmtr_val, 1)) >= 9
+      THEN
+        TRUE  -- Hydromorphone base substance (strengths equal to or higher than 9mg) (ing code)
+    ELSE FALSE
+    END AS is_high_strength
 FROM vmp_amp
 INNER JOIN dmd.vmp vmp
   ON vmp.id = vmp_amp.vmp
