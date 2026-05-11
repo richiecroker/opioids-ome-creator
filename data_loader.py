5e318069c41ec617876505070ba69ea6227eee68
Richard Croker <ra.croker@gmail.com>
2026-05-11 16:33:02 +0100

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -20,7 +20,6 @@ def load_sql(path: str) -> str:
 # Load your specific query
 ome_sql = load_sql("sql/ome.sql")
 date_sql = load_sql("sql/max_month.sql")
-high_dose_la_sql = load_sql("sql/high_dose_la.sql")
 la_sql = load_sql("sql/la.sql")
 
 # Generic cached query runner
